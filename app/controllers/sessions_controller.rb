a3f15d687fccea248dd3ae55bd4150ec398b70ae
Nick Lisauskas <nlisausk@alumni.nd.edu>
2018-05-08 02:28:53 +0000

    automatically backed up by learn

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,7 +1,10 @@
 class SessionsController < ApplicationController
   def create
     resp = Faraday.post('https://github.com/login/oauth/access_token') do |req|
-      
+      req.params["client_id"] = ENV["CLIENT_ID"]
+      req.params["client_secret"] = ENV["CLIENT_SECRET"]
+      req.params["code"] = params["code"]
+      req.headers["Accept"] = "applications/json"
     end
   end
 end
