235650e648196d25ab24fac42851140167f5c913
Nick Lisauskas <nlisausk@alumni.nd.edu>
2018-05-08 02:41:30 +0000

    automatically backed up by learn

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,14 +1,17 @@
-class SessionsController < ApplicationController
-  def create
-    resp = Faraday.post('https://github.com/login/oauth/access_token') do |req|
-      req.params["client_id"] = ENV["CLIENT_ID"]
-      req.params["client_secret"] = ENV["CLIENT_SECRET"]
-      req.params["code"] = params["code"]
-      req.headers["Accept"] = "applications/json"
-    end
 
-    session[:token] = JSON.parse(resp.body)["access_token"]
-    redirect_to root_path
-  
+class SessionsController < ApplicationController
+  skip_before_action :authenticate_user
+
+  def create
+    response = Faraday.post "https://github.com/login/oauth/access_token", {client_id: ENV["GITHUB_CLIENT"], client_secret: ENV["GITHUB_SECRET"],code: params[:code]}, {'Accept' => 'application/json'}
+    access_hash = JSON.parse(response.body)
+    session[:token] = access_hash["access_token"]
+
+    user_response = Faraday.get "https://api.github.com/user", {}, {'Authorization' => "token #{session[:token]}", 'Accept' => 'application/json'}
+    user_json = JSON.parse(user_response.body)
+    session[:username] = user_json["login"]
+
+    redirect_to '/'
   end
+
 end
