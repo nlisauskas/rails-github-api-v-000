2bd92b68fd3e905553e3a70444fed19e8b1bae8b
Nick Lisauskas <nlisausk@alumni.nd.edu>
2018-05-08 02:36:28 +0000

    automatically backed up by learn

--- app/controllers/repositories_controller.rb
+++ app/controllers/repositories_controller.rb
@@ -1,5 +1,14 @@
 class RepositoriesController < ApplicationController
   def index
+    resp = Faraday.get('https://api.github.com/user/repos') do |req|
+      req.headers["Authorization"] = "token #{session[:token]}"
+    end
+
+    @repos = JSON.parse(resp.body)
+
+    user_resp = Faraday.get('https://api.github.com/user') do |req|
+      req.headers["Authorization"] = "token #{session[:token]}"
+    end
   end
 
   def create
